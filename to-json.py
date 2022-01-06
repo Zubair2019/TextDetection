abec4b8ca6dd590c44752e971584d3cf4d70ea92
Zubair2019 <zikayem@gmail.com>
2022-01-06 22:37:51 +0530

    final to-json 06/01

--- to-json.py
+++ to-json.py
@@ -9,25 +9,34 @@ from pytesseract import Output
 from tqdm import tqdm
 import datetime
 import time
+import numpy as np
 
-
+''' This function takes as input the path of the xml file and deletes
+the unnecessary information and saves the output as a json file
+'''
 def parseXML(path):
-    final_dict = {}
+    # open the xml file for reading
     with open(path) as xml_file:
         data_dict = xmltodict.parse(xml_file.read())
     xml_file.close()
     object_length = len(data_dict["annotation"]["object"])
     # print(object_length)
+
+    # The following del statements delete the unnecessary information
     del data_dict["annotation"]["folder"]
     del data_dict["annotation"]["filename"]
     del data_dict["annotation"]["source"]
     del data_dict["annotation"]["size"]
     del data_dict["annotation"]["segmented"]
+    
+    #The following code block created a directory for each image to store individual crops
     try:
         temp_path = str(path).replace('images','temp').split('.')[0]
         os.mkdir(temp_path)
     except OSError as error:
         print(error)
+
+    # for loop for deleting and adding info to json
     for j in range(object_length):
         # del data_dict["annotation"]["object"][j]["name"]
         del data_dict["annotation"]["object"][j]["pose"]
@@ -38,28 +47,47 @@ def parseXML(path):
             int(data_dict["annotation"]["object"][j]["bndbox"]["ymin"]),\
                 int(data_dict["annotation"]["object"][j]["bndbox"]["xmax"]),\
                     int(data_dict["annotation"]["object"][j]["bndbox"]["ymax"]),
-        data_dict["annotation"]["object"][j]["name"] = cropping(data_dict["annotation"]["path"],xmin,ymin,xmax-xmin,ymax-ymin,j)
+        data_dict["annotation"]["object"][j]["name"] = cropping(data_dict["annotation"]["path"],xmin,ymin,xmax-xmin,ymax-ymin)
 
 
     json_data = json.dumps(data_dict)
-     
     # Write the json data to output
-    # json file
     mypath = os.path.realpath(__file__)
     mypath = mypath.replace('to-json.py','output/')
     # print(mypath)
     mypath = mypath+str(path).split('.')[0].split('/')[-1]+'.json'
     # print(mypath)
+
+    '''Write the json file'''
     with open(mypath, "w") as json_file:
         json_file.write(json_data)
         json_file.close()
 
-def cropping(FILE, x,y,width,height,j): #Funtion to crop image and return multiple cropped images
+'''The following function takes as input.
+1.  FILE: The input image path.
+2.  x: The x coordinate of top left corner of the cropped box.
+3.  y: The y coordinate of top left corner of the cropped box.
+4.  width:  the width of the cropped box.
+5.  height: the height of the cropped box.
+
+Saves the crooped image with the name of the detected text in the temp folder.
+Also returns the detected text as output.
+'''
+
+def cropping(FILE, x,y,width,height): #Funtion to crop image and return multiple cropped images
     # print(FILE)
     img = cv2.imread(FILE)
     crop_img = img[y:y + height, x:x + width]
+    gray = cv2.cvtColor(crop_img, cv2.COLOR_BGR2GRAY)
+    # gray = blur(gray)
+    if check_blackness(gray) == True:
+        gray = cv2.adaptiveThreshold(gray,255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY_INV,11,5)
+    else:
+        gray = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 21, -1)
+    
+    gray = cv2.bitwise_not(gray)
     config = ('-l eng --oem 1 --psm 8')
-    results = pytesseract.image_to_data(crop_img, config=config, output_type=Output.DICT)
+    results = pytesseract.image_to_data(gray, config=config, output_type=Output.DICT)
     # print(results)
 
     for i in range(0, len(results["text"])):
@@ -69,9 +97,20 @@ def cropping(FILE, x,y,width,height,j): #Funtion to crop image and return multip
         #     print("Confidence: {}".format(conf))
         #     print("Text: {}".format(string))
         temp_path1 = str(FILE).replace('images','temp').split('.')[0]
-    cv2.imwrite(temp_path1+'/'+string+".jpg", crop_img)
+    cv2.imwrite(temp_path1+'/'+string+".jpg", gray)
 
     return string
+
+def check_blackness(image):
+    # print("Total pixels:",image.size)
+    # print("White Pixels:",np.sum(image >= 125))
+    # print("Black Pixels:", np.sum(image == 0))
+    if np.sum(image >= 127)/image.size > 0.7:
+        # print("Image is White")
+        return True
+    else:
+        # print("Black image")
+        return False
 
 # Driver Code
 if __name__ == "__main__":
@@ -86,4 +125,4 @@ if __name__ == "__main__":
         parseXML(item)
     end_time = time.time()
     # print("End Time: ",end_time)
-    print("Done!!!   Time Taken: ", end_time-cur_time)
+    print("Done!!!   Time Taken: ", round(end_time-cur_time), "Seconds")
