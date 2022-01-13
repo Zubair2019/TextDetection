be56f05d84f03db25954ffbb71b972040b464676
Zubair2019 <zikayem@gmail.com>
2022-01-13 17:22:16 +0530

    added colors

--- compare.py
+++ compare.py
@@ -2,41 +2,47 @@ import os
 import glob
 from tqdm import tqdm
 import json
+from colors import *
 
+# Function reads the json files from output and annotated folder for comparison
 def read_json():
-	dict1 = {}
 	mypath = os.path.realpath(__file__)
-	gtpath = mypath.replace('compare.py','output/')
-	genpath = mypath.replace('compare.py','annotated/')
+	gtpath = mypath.replace('compare.py','output/') #Ground truth files are in output folder 
+	genpath = mypath.replace('compare.py','annotated/') # Generated files are in annotted folder
 	lst1 = glob.glob(gtpath+"*.json")
 	lst2 = glob.glob(genpath+"*.json")
 	for item1,item2 in zip(lst1,lst2):
 		if str(item1).split('.')[0].split('/')[-1] == str(item2).split('.')[0].split('/')[-1]:
-			print("File name Matches")
+			# print("File name Matches")
 			with open(item1, "r") as json_file:
 				dict1 = json.load(json_file)
 			with open(item2, "r") as json_file:
 				dict2 = json.load(json_file)
-			compare_text(dict1,dict2)
+			correct, incorrect = compare_text(dict1,dict2)
+			print(bcolors.WARNING+'File:'+ str(item2)+bcolors.ENDC,bcolors.OKGREEN+'Correct:--'+str(correct),bcolors.FAIL+bcolors.BOLD+'Incorrect:--'+str(incorrect)+bcolors.ENDC)
 		else:
 			print("File name doesn't Match")
 		
-def compare_text(dic1,dic2):
+
+		#Function to compare dic1 and dic2 and return the correct and incorrect instances of text
+def compare_text(dic1,dic2): 
 	count = 0
-	for k in range(2):
+	for k in range(2): #second iteration to look for multiple occurences of a word
 		for i in range (len(dic1["text"])):
 			# print("DICT---",dic1,'------',dic2)
 			if dic1["text"][i] in dic2["text"]:
 				j = list(dic2["text"]).index(dic1["text"][i])
 				box_a = [dic1["left"][i],dic1["top"][i],dic1["left"][i]+dic1["width"][i],dic1["top"][i]+dic1["height"][i]]
 				box_b = [dic2["left"][j],dic2["top"][j],dic2["left"][j]+dic2["width"][j],dic2["top"][j]+dic2["height"][j]]
-				print(i,j,dic1["text"][i],dic2["text"][j],box_a,box_b, bb_intersection_over_union(box_a,box_b))
+				# print(i,j,dic1["text"][i],dic2["text"][j],box_a,box_b, bb_intersection_over_union(box_a,box_b))
 				if bb_intersection_over_union(box_a,box_b) >0.30:
-					# del dic1["left"][i],dic1["top"][i],dic1["width"][i],dic1["height"][i],dic1["text"][i]
 					del dic2["left"][j],dic2["top"][j],dic2["width"][j],dic2["height"][j],dic2["text"][j]
-				# print(i,j,dic1["text"][i],dic2["text"][j],box_a,box_b,bb_intersection_over_union(box_a,box_b))
 					count += 1
-	print(count,len(dic2["text"]),len(dic1["text"]))
+	# print(count,len(dic2["text"]),len(dic1["text"])) # 'len(dic2["text"]' represents incorect detections
+	return count, len(dic2["text"])
+
+
+# Function to find the intersection over union of two boxes
 def bb_intersection_over_union(boxA, boxB):
 	# determine the (x, y)-coordinates of the intersection rectangle
 	xA = max(boxA[0], boxB[0])
@@ -55,10 +61,6 @@ def bb_intersection_over_union(boxA, boxB):
 	iou = interArea / float(boxAArea + boxBArea - interArea)
 	# return the intersection over union value
 	return iou
-
-
-
-
 
 
 if __name__ == '__main__':
